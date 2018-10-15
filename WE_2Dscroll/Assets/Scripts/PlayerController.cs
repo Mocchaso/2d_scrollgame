f327963e7adc6ae81a8903e48e77cdabdb91b573
Mocchaso <saka.ksftn2718@gmail.com>
2018-10-15 13:44:49 +0900

    openbookの実装：ジャンプまで完了。(ジャンプの動きがまだ不自然)

---
+++ WE_2Dscroll/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	private float speed = 10;
+	private bool grounded = false;
+	public Rigidbody2D rigidbody2D;
+	public LayerMask groundlayer;
+
+	// Use this for initialization
+	void Start () {
+		// PlayerのRotationを固定
+		rigidbody2D.fixedAngle = true;		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// 接地しているかどうかを調べる
+		grounded = Physics2D.Linecast(transform.position, transform.position - transform.up * 1.2f, groundlayer);
+		if (Input.GetKeyDown("space") && grounded) {
+			// ジャンプ
+			rigidbody2D.AddForce(Vector2.up * 30000f);
+		}
+
+		// 左右のキー入力を受け取る
+		float x = Input.GetAxisRaw("Horizontal");
+
+		// 移動する向きを求める
+		Vector2 direction = new Vector2(x, 0).normalized;
+
+		// 移動するスピードを求めて代入する
+		rigidbody2D.velocity = direction * speed;
+	}
+}
